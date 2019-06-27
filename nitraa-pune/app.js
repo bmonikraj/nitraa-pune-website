8d094ff37a20059a015142abe371c03fc1e54d31
MONIK RAJ BEHERA <bmonikraj@gmail.com>
2019-06-28 00:32:52 +0530

    Update app.js

--- nitraa-pune/app.js
+++ nitraa-pune/app.js
@@ -35,9 +35,9 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+// app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
 
 app.use('/login-admin', loginAdminRouter);
 app.use('/admin-moderator-crud', adminModeratorCRUDRouter);
