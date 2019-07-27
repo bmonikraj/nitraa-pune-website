b5b17cd1781fca6f615d83ce4f0329d44b627035
ajit456behera <35414172+ajit456behera@users.noreply.github.com>
2019-07-27 18:38:40 +0530

    webhook event registration

---
+++ nitraa-pune/routes/event_registration_webhook/index.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+    path = require('path'),
+    mongo = require('mongodb').MongoClient,
+    ObjectID = require('mongodb').ObjectID,
+    bodyParser = require('body-parser'),
+    app = express();
+var jwt = require('jsonwebtoken');
+var jwt_salt = require('../constant/jwt/index');
+var urlMongo = require('../constant/mongodbAddress/index');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+const request = require('request');
+router = express.Router();
+
+router.post('/payment-webhook', function(req, res, next) {
+    mongo.connect(urlMongo, {useNewUrlParser : true}, function (err_mdbcon, db) {
+      if (err_mdbcon == null) {
+        let dbo = db.db('nitraapune');
+        if(req.body.payment_id != "" && req.body.payment_request_id != "" && req.body.status == "Credit"){
+          dbo.collection('event_registration').updateOne({ '_id': ObjectID(req.body.purpose) }, {
+            $set:{
+              payment_details: req.body,
+              payment_status: "paid"
+            }
+          }, (err1, res1) => {
+            if(err1){
+              db.close();
+              res.json({status: "fail", message: "Database Error"});
+            }
+            else{
+              db.close();
+              res.json({status: "success", message: "Successfully Updated"});
+            }
+          });
+        }
+      }
+      else{
+        res.json({status: 'fail', message: 'Database connection failed'});
+      }
+    });
+});
+module.exports = router;
