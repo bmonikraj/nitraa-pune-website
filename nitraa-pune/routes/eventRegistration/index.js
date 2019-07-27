f90402bb3bb08a564a36a55aac29b4b942173ac4
ajit456behera <35414172+ajit456behera@users.noreply.github.com>
2019-07-27 18:40:03 +0530

    minor changes

--- nitraa-pune/routes/eventRegistration/index.js
+++ nitraa-pune/routes/eventRegistration/index.js
@@ -165,33 +165,6 @@ router.post('/payment', function(req, res, next) {
     }
 });
 
-router.post('/payment-webhook', function(req, res, next) {
-    mongo.connect(urlMongo, {useNewUrlParser : true}, function (err_mdbcon, db) {
-      if (err_mdbcon == null) {
-        let dbo = db.db('nitraapune');
-        if(req.body.payment_id != "" && req.body.payment_request_id != "" && req.body.status == "Credit"){
-          dbo.collection('event_registration').updateOne({ '_id': ObjectID(req.body.purpose) }, {
-            $set:{
-              payment_details: req.body,
-              payment_status: "paid"
-            }
-          }, (err1, res1) => {
-            if(err1){
-              db.close();
-              res.json({status: "fail", message: "Database Error"});
-            }
-            else{
-              db.close();
-              res.json({status: "success", message: "Successfully Updated"});
-            }
-          });
-        }
-      }
-      else{
-        res.json({status: 'fail', message: 'Database connection failed'});
-      }
-    });
-});
 /******************************************Event Registration ends*************************************/
 
 /********************************************Registered User fetch for a particular event begins**************************************************/
